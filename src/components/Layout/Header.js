b85ddcf7b3683b218613dde68a6c4179bb1c2d1f
Arslan <ali.arslan319@gmail.com>
2022-01-25 23:39:59 -0500

    initial Header component creation

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+const Header = props => {
+  return (
+    <>
+    <header>
+      <h1>ReactMeals</h1>
+      <button>Cart</button>
+    </header>
+    <div></div>
+    </>
+  )
+}
+
+export default Header 
