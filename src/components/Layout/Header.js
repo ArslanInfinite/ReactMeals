0260f38fcd92741a332f7dfeca01b740b081f694
Arslan <ali.arslan319@gmail.com>
2022-01-26 23:53:40 -0500

    create and populate HeaderCartButton with css attributes

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,13 +4,14 @@ import classes from './Header.module.css'
 const Header = props => {
   return (
     <>
-    <header className={classes.header}> 
-      <h1>ReactMeals</h1>
-      <button>Cart</button>
-    </header>
-    <div className={classes=['main-image']}>
-      <img src={epicMeal} alt='The funniest pic in human history'/>
-    </div>
+      <header className={classes.header}> 
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+
+      <div className={classes.mainImage}>
+        <img src={epicMeal} alt='The funniest pic in human history'/>
+      </div>
     </>
   )
 }
