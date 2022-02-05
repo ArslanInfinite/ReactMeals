f784a749fb35c9fb7ecbff89d686fa9cde0b52ee
Arslan <ali.arslan319@gmail.com>
2022-02-04 23:58:53 -0500

    convert price props to two decimal space display

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,12 @@
 const MealItem = props => {
+
+  const price = `$${props.price.toFixed(2)}`
   return (
     <li>
       <div>
         <h3>{props.name}</h3>
         <div>{props.description}</div>
-        <div>{props.price}</div>
+        <div>{price}</div>
         </div>
       <div></div>
     </li>
