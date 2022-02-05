0850f309f47095005bb6a16e90a935dbbb44a540
Arslan <ali.arslan319@gmail.com>
2022-02-04 23:57:45 -0500

    display props anme, description, and price

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,11 @@
 const MealItem = props => {
   return (
     <li>
-      <div><h3>{props.name}</h3></div>
+      <div>
+        <h3>{props.name}</h3>
+        <div>{props.description}</div>
+        <div>{props.price}</div>
+        </div>
       <div></div>
     </li>
   )
